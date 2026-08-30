c9105e668e978cba447142a341908eab94ed38d2
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-08-30 11:53:42 +0300

    The  custom logoutview

--- users/views.py
+++ users/views.py
@@ -2,9 +2,38 @@ from .serializers import Signup
 from .models import User
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt import token_blacklist
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+
 
 class SignUpView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = Signup
+    
+class LogoutView(APIView):
+    permission_classes = IsAuthenticated
+    
+    def post(self, request):
+        
+        try:
+            refresh_token = request.data['refersh']
+            token = RefreshToken(refresh_token)
+            
+            token.blacklist()
+            
+            return Response({
+                "message": "Successfully logged out."
+            }, status=status.HTTP_205_RESET_CONTENT)
+            
+        except Exception:
+            return Response({
+                "message":"Invalid request or token"
+            }, status=status.HTTP_400_BAD_REQUEST)    
+        
+        
 
 
