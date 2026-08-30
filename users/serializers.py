8dff0e60facff8411a7ddc7d23b669a66c8f83af
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-08-30 10:07:34 +0300

    User SignUp serializer created

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Signup(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    
+    class Meta:
+        model = User
+        fields = ['id','username', 'email', 'password', 'role']
+        
+    def create(self, validated_data):
+        
+        username = validated_data['username']
+        email= validated_data['email']
+        password = validated_data['password']
+        role =  validated_data.get('role')
+        
+        user = User.objects.create_user(**validated_data)
+        
+        return user
+        
+        
+            
