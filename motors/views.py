79da193ca042249f9d4e036d59deb747231270e4
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-09-08 22:58:48 +0300

    CreateMotorApiView

--- motors/views.py
+++ motors/views.py
@@ -6,4 +6,16 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics
 from .models import Motor
 
-class CreateMotorApiView()
+class CreateMotorApiView(generics.CreateAPIView):
+    
+    def post(self, request):
+        
+        serializer = MotorSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    
+    
