b5d511a4577f05e5dcdc376df5d82def9c19cc2e
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-09-09 18:05:35 +0300

    Booking view for the bookins app

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Booking
+from .serializers import BookingSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class BookingApiView(CreateAPIView):
+    
+    
+    serializer_class = BookingSerializer
+    
+    def post(self, request):
+        
+        serializer = BookingSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save(client=request.user)
+            return Response({
+                'data':serializer.data
+            })
+        
+        return Response({
+            'errors':serializer.data
+        })
+        
+            
