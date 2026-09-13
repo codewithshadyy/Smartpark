7dfe72c87b7b42139aa30da8fa48c6eacd02657f
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-09-13 23:34:24 +0300

    client RBAC

--- users/permissions.py
+++ users/permissions.py
@@ -14,8 +14,15 @@ class IsAdminUser(BasePermission):
 class IsAttendantUser(BasePermission):
     
     def has_permission(self, request, view):
-        return bool(
-            request.user and
-            request.user.is_authenticated and
-            request.user.role in ["Admin", "Attendant"]
-        )
+        if not (request.user and request.user.is_authenticated):
+            return False
+        
+        return request.user.role in ['admin', 'attendant']
+
+class IsClientUser(BasePermission):
+  
+    def has_permission(self, request, view):
+        if not (request.user and request.user.is_authenticated):
+            return False
+            
+        return request.user.role in ['admin', 'client']
