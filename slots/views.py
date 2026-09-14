edc5ccc8a914b3a6492ffae402a1afc5f5340033
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-09-14 17:52:35 +0300

    Used Generics in the CreatSlotAPIVew not the viewsets agaian

--- slots/views.py
+++ slots/views.py
@@ -3,11 +3,12 @@ from .models import Slot
 from .serializers import SLotSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from  rest_framework import generics
+from users.permissions import IsAdminUser, IsAttendantUser, IsClientUser
 
 
-class SlotViewSet(viewsets.ModelViewSet):
-    
+class CreateSLotApiView(generics.CreateAPIView):
     
     queryset = Slot.objects.all()
     serializer_class = SLotSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
